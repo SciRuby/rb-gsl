e87bb6c5ddaf2f44446c0371a7cc36c737865a49
David MacMahon <davidm@astro.berkeley.edu>
2010-11-15 04:19:51 +0000

    Hack rdtordoc.rb for one-time conversion of rb-gsl
    This includes several hacks to facilitate the onetime conversion of rd
    docs to rdoc docs.  It includes things like hacking in anchor names to
    rdoc headings and simplifying irb prompts.
    
    Manual tweaking will need to be done on the generated rdoc files, but I
    think this script is to the point where time will be better spent
    tweaking the generated rdocs than tweaking this script to handled
    everything perfectly.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rb-gsl/trunk/rb-gsl@125 6e764f74-f39f-46f8-8c54-8307d62afe8d

--- rdtordoc.rb
+++ rdtordoc.rb
@@ -13,19 +13,44 @@ class RdToRdocError < RuntimeError; end
 class RdToRdoc
   OUTSIDE_COMMENT = :OUTSIDE_COMMENT
   INSIDE_COMMENT = :INSIDE_COMMENT
-  @@mapping = { 
-    /^(\s*)\((\d+)\)/ => '\1\2', # numbered list
-    /^(\s*):(\s*)(\S+)/ => '\1[\2]', # labelled list
-    /^(\s*)\-{3}/ => '\1   ', # line contains a method. Rdoc should cope
-    /\(\(\*(.*?)\*\)\)/ => '<em>\1</em>',
-    /\(\(\{(.*?)\}\)\)/ => '<tt>\1</tt>',
-    /\(\(\|(.*?)\|\)\)/ => '#\1',
-    /\(\(\%(.*?)\%\)\)/ => '<tt>\1</tt>', # Kbd input
-    /\(\(\:(.*?)\:\)\)/ => '\1', # Index, not supported.
-    /\(\(\<(.*?)\>\)\)/ => '\1', # Hyperlink, auto detected by rdoc
-    /\(\(\-(.*?)\-\)\)/ => '(Note: \1)', # Footnote, not supported.
-    /\(\(\'(.*?)\'\)\)/ => '(Note: \1)'  # Verbatim text.
-  }
+  @@mapping = [ 
+    # numbered list
+    #[/^(\s*)\((\d+)\)/, '\1\2.'],
+    [/^(\s*)\((\d+)\)/, '1.'],
+    # labelled list
+    [/^(\s*):(\s*)(\S+)/, '\1[\2]'],
+    # line contains a method. indent
+    [/^(\s*)\-{3}\s/, '\1 '],
+    # Emphasis
+    [/\(\(\*/, '<b>'], [/\*\)\)/, '</b>'], # em?
+    # Code
+    [/\(\(\{/, '<tt>'], [/\}\)\)/, '</tt>'],
+    # Variable
+    [/\(\(\|/, '<tt>'], [/\|\)\)/, '</tt>'], # em?
+    # Kbd input
+    [/\(\(\%/, '<tt>'], [/\%\)\)/, '</tt>'],
+    # Index, not supported.
+    [/\(\(\:/, ''], [/\:\)\)/, ''],
+    # Non-image hyperlink, auto detected by rdoc, use target=_top trick to open
+    # external links in top level frame.
+    [/\(\(\<(.*?)\|URL:http:(.*?)\>\)\)/, '{\1}[http:\2"target="_top]'],
+    # Image hyperlink, auto detected by rdoc, use target=_top trick to open
+    # external links in top level frame.
+    [/\(\(\<(.*?)\|"IMG:http:(.*?)"\>\)\)/, '{\1}[http:\2]'],
+    # Local hyperlink to top index
+    # TODO: use link: and _top trick
+    # Local HTML hyperlink, auto detected by rdoc
+    # Add _rdoc suffix to stem
+    [/\(\(\<(.*?)\|URL:(.*?).html([^>]*)\>\)\)/, '{\1}[link:files/\2_rdoc.html\3]'],
+    # Local non-HTML hyperlink, auto detected by rdoc
+    [/\(\(\<(.*?)\|URL:(.*?)\>\)\)/, '{\1}[link:files/\2]'],
+    # Footnote, not supported.
+    [/\(\(\-/, '(Note: '], [/\-\)\)/, ')'],
+    # Verbatim text.
+    [/\(\(\'/, '<tt>'], [/\'\)\)/, '</tt>'],
+    # IRB reformatting
+    [/irb\(.*?\):\d+:\d+>/, '>>']
+  ]
 
   # The constructor takes an input object that responds to readline,
   # and an output object that responds to puts.  Use is made of
@@ -41,19 +66,46 @@ class RdToRdoc
   end
 
   def process()
+    header_levels = [0]
+    strip_mode = false
+    method_block = false
     while line = @input.gets
       case @state
       when INSIDE_COMMENT
         if line =~ /^=end/
+          line = ''
           @state=OUTSIDE_COMMENT
         else
-          @@mapping.each do |key,value|
-            line.gsub!(key, value)
+          # Non-title heading lines
+          if line =~ /^=(=+)\s+(.*)/
+            header_levels += [0] until header_levels.length >= $1.length
+            header_levels = header_levels[0, $1.length]
+            header_levels[-1] += 1
+            line = %Q{=#{$1} {}[link:files/index_rdoc.html"name="#{header_levels.join('.')}] #{$2}}
+          end
+          # If method line, turn on "strip mode", which strips off 4 leading
+          # spaces.
+          if line =~ /^---\s/
+            # Put blank line before blocks of methods
+            @output.puts if !method_block
+            strip_mode = true
+            method_block = true
+          elsif line =~ /^=/
+            strip_mode = false
+            method_block = false
+          elsif strip_mode
+            # Strip leading whitespace before mapping
+            line.gsub!(/^\s{4}/,'')
+            method_block = false
+          end
+          @@mapping.each do |pattern,replacement|
+            line.gsub!(pattern, replacement)
           end
         end
       when OUTSIDE_COMMENT
         if line =~ /^=begin/
-          line.gsub!(/^=begin/, '=begin rdoc')
+          #line.gsub!(/^=begin/, '=begin rdoc')
+          line = ''
           @state=INSIDE_COMMENT
         end
       else
