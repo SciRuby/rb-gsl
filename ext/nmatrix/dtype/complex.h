353be3235b270ffdacaa4bb6e9843be95640036b
Chris Wailes <chris.wailes@gmail.com>
2012-06-29 15:11:54 -0600

    Added some cast operators.  Added operator definitions for left hand native types.  Changed the name of some identifiers.

--- ext/nmatrix/dtype/complex.h
+++ ext/nmatrix/dtype/complex.h
@@ -40,20 +40,20 @@
  * Macros
  */
 
-#define COMPLEX_COMPLEX_OPS(other_type) 																																\
-	inline Complex<Type> operator+(const Complex<other_type>& other) const {															\
+#define COMPLEX_COMPLEX_OPS(OtherType) 																																	\
+	inline Complex<Type> operator+(const Complex<OtherType>& other) const {																\
 		return Complex<Type>(this->r + other.r, this->i + other.i);																					\
 	}																																																			\
 																																																				\
-	inline Complex<Type> operator-(const Complex<other_type>& other) const {															\
+	inline Complex<Type> operator-(const Complex<OtherType>& other) const {																\
 		return Complex<Type>(this->r - other.r, this->i - other.i);																					\
 	}																																																			\
 																																																				\
-	inline Complex<Type> operator*(const Complex<other_type>& other) const {															\
+	inline Complex<Type> operator*(const Complex<OtherType>& other) const {																\
 		return Complex<Type>(this->r * other.r - this->i * other.i, this->r * other.i - this->i * other.r);	\
 	}																																																			\
 																																																				\
-	inline Complex<Type> operator/(const Complex<other_type>& other) const {															\
+	inline Complex<Type> operator/(const Complex<OtherType>& other) const {																\
 		Type new_r, new_i;																																									\
 		Type denom = this->i * this->i + other.r * other.r;																									\
 																																																				\
@@ -63,44 +63,48 @@
 		return Complex<Type>(new_r, new_i);																																	\
 	}																																																			\
 																																																				\
-	inline bool operator<(const Complex<other_type>& other) const {																				\
+	inline bool operator<(const Complex<OtherType>& other) const {																				\
 		return (this->r < other.r) || ((this->r <= other.r) && (this->i < other.i));												\
 	}																																																			\
 																																																				\
-	inline bool operator>(const Complex<other_type>& other) const {																				\
+	inline bool operator>(const Complex<OtherType>& other) const {																				\
 		return (this->r > other.r) || ((this->r >= other.r) && (this->i > other.i));												\
 	}																																																			\
 																																																				\
-	inline bool operator==(const Complex<other_type>& other) const {																			\
+	inline bool operator==(const Complex<OtherType>& other) const {																				\
 		return FP_EQUAL(this->r, other.r) && FP_EQUAL(this->i, other.i);																		\
 	}																																																			\
 																																																				\
-	inline bool operator!=(const Complex<other_type>& other) const {																			\
+	inline bool operator!=(const Complex<OtherType>& other) const {																				\
 		return !(*this == other);																																						\
 	}																																																			\
 																																																				\
-	inline bool operator<=(const Complex<other_type>& other) const {																			\
+	inline bool operator<=(const Complex<OtherType>& other) const {																				\
 		return (*this < other) || (*this == other);																													\
 	}																																																			\
 																																																				\
-	inline bool operator>=(const Complex<other_type>& other) const {																			\
+	inline bool operator>=(const Complex<OtherType>& other) const {																				\
 		return (*this > other) || (*this == other);																													\
 	}																																																			\
+																																																				\
+	inline operator Complex<OtherType> () {																																\
+		return Complex<OtherType>((OtherType)this->r, (OtherType)this->i);																	\
+	}
 
-#define COMPLEX_NATIVE_OPS(type) 																	\
-	inline Complex<Type> operator+(const other_type& other) const {	\
+#define COMPLEX_NATIVE_OPS(NativeType)														\
+	inline Complex<Type> operator+(const NativeType& other) const {	\
 		return Complex<Type>(this->r + other, this->i);								\
 	}																																\
 																																	\
-	inline Complex<Type> operator-(const other_type& other) const {	\
+	inline Complex<Type> operator-(const NativeType& other) const {	\
 		return Complex<Type>(this->r - other, this->i);								\
 	}																																\
 																																	\
-	inline Complex<Type> operator*(const other_type& other) const {	\
+	inline Complex<Type> operator*(const NativeType& other) const {	\
 		return Complex<Type>(this->r * other, this->i * other);				\
 	}																																\
 																																	\
-	inline Complex<Type> operator/(const other_type& other) const {	\
+	inline Complex<Type> operator/(const NativeType& other) const {	\
 		Type new_r, new_i;																						\
 		Type denom = this->i * this->i + other * other;								\
 																																	\
@@ -110,29 +114,84 @@
 		return Complex<Type>(new_r, new_i);														\
 	}																																\
 																																	\
-	inline bool operator<(const other_type& other) const {					\
+	inline bool operator<(const NativeType& other) const {					\
 		return this->r < other;																				\
 	}																																\
 																																	\
-	inline bool operator>(const other_type& other) const {					\
+	inline bool operator>(const NativeType& other) const {					\
 		return this->r > other.r;																			\
 	}																																\
 																																	\
-	inline bool operator==(const other_type& other) const {					\
-		return this->r == other;																			\
+	inline bool operator==(const NativeType& other) const {					\
+		return (this->r == other) && FP_IS_ZERO(this->i);							\
 	}																																\
 																																	\
-	inline bool operator!=(const other_type& other) const {					\
-		return this->r != other;																			\
+	inline bool operator!=(const NativeType& other) const {					\
+		return !(*this == other);																			\
 	}																																\
 																																	\
-	inline bool operator<=(const other_type& other) const {					\
-		return this->r <= other;																			\
+	inline bool operator<=(const NativeType& other) const {					\
+		return (*this < other) || (*this == other);										\
 	}																																\
 																																	\
-	inline bool operator>=(const other_type& other) const {					\
-		return this->r >= other;																			\
+	inline bool operator>=(const NativeType& other) const {					\
+		return (*this > other) || (*this == other);										\
 	}																																\
+																																	\
+	inline operator NativeType () {																	\
+		return (NativeType)this->r;																		\
+	}
+
+#define NATIVE_COMPLEX_OPS(NativeType, ComplexType)																										\
+	inline Complex<ComplexType>& operator+(const NativeType& left, const Complex<ComplexType>& right) {	\
+		return Complex<ComplexType>(left + right.r, right.i);																							\
+	}																																																		\
+																																																			\
+	inline Complex<ComplexType>& operator-(const NativeType& left, const Complex<ComplexType>& right) {	\
+		return Complex<ComplexType>(left - right.r, right.i);																							\
+	}																																																		\
+																																																			\
+	inline Complex<ComplexType>& operator*(const NativeType& left, const Complex<ComplexType>& right) {	\
+		return Complex<ComplexType>(left * right.r, left * right.i);																			\
+	}																																																		\
+																																																			\
+	inline Complex<ComplexType>& operator/(const NativeType& left, const Complex<ComplexType>& right) {	\
+		ComplexType new_r, new_i;																																					\
+		ComplexType denom = right.r * right.r;																														\
+																																																			\
+		new_r = (left * right.r) / denom;																																	\
+		new_i = (left * right.i) / denom;																																	\
+																																																			\
+		return Complex<Type>(new_r, new_i);																																\
+	}																																																		\
+																																																			\
+	inline bool operator<(const NativeType left, const Complex<ComplexType>& right) {										\
+		return left < right.r;																																						\
+	}																																																		\
+																																																			\
+	inline bool operator>(const NativeType left, const Complex<ComplexType>& right) {										\
+		return left > right.r;																																						\
+	}																																																		\
+																																																			\
+	inline bool operator==(const NativeType left, const Complex<ComplexType>& right) {									\
+		return (left == right.r) && FP_IS_ZERO(right.i);																									\
+	}																																																		\
+																																																			\
+	inline bool operator!=(const NativeType left, const Complex<ComplexType>& right) {									\
+		return !(left == right);																																					\
+	}																																																		\
+																																																			\
+	inline bool operator<=(const NativeType left, const Complex<ComplexType>& right) {									\
+		return (left < right) || (left == right);																													\
+	}																																																		\
+																																																			\
+	inline bool operator>=(const NativeType left, const Complex<ComplexType>& right) {									\
+		return (left > right) || (left == right);																													\
+	}																																																		\
+																																																			\
+	inline operator Complex<ComplexType> (NativeType nv) {																							\
+		return Complex<ComplexType>(nv, 0);																																\
+	}
 
 /*
  * Types
@@ -185,5 +244,21 @@ class Complex {
 		COMPLEX_NATIVE_OPS(float)
 		COMPLEX_NATIVE_OPS(double)
 };
+
+NATIVE_COMPLEX_OPS(char,					float)
+NATIVE_COMPLEX_OPS(unsigned char,	float)
+NATIVE_COMPLEX_OPS(short,					float)
+NATIVE_COMPLEX_OPS(int,						float)
+NATIVE_COMPLEX_OPS(long,					float)
+NATIVE_COMPLEX_OPS(float,					float)
+NATIVE_COMPLEX_OPS(double,				float)
+
+NATIVE_COMPLEX_OPS(char,					double)
+NATIVE_COMPLEX_OPS(unsigned char,	double)
+NATIVE_COMPLEX_OPS(short,					double)
+NATIVE_COMPLEX_OPS(int,						double)
+NATIVE_COMPLEX_OPS(long,					double)
+NATIVE_COMPLEX_OPS(float,					double)
+NATIVE_COMPLEX_OPS(double,				double)
 
 #endif
