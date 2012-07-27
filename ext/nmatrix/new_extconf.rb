48a82a7e723b52a2e2784e460a51d607ff9546c1
Chris Wailes <chris.wailes@gmail.com>
2012-07-27 11:36:23 -0600

    Changed new_extconf.rb to work with clang/clang++.  Still needs some work if we want to support LTO.

--- ext/nmatrix/new_extconf.rb
+++ ext/nmatrix/new_extconf.rb
@@ -27,31 +27,20 @@
 require 'mkmf'
 
 # Configure our compilers and compiler options.
-#
-# FIXME: Get this to work with clang and clang++.
+$CFLAGS							= '-I. -fPIC -Wall -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector -mtune=native -O3'
+CONFIG['CXXFLAGS']	= '-std=c++11'
 
-#$CFLAGS							= '-I. -fPIC -Wall -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector -mtune=native'
-CONFIG['CXXFLAGS']	= '-std=c++11 -O3'
-
-#if clang_pp_path = find_executable('clang++')
-#	CONFIG['CC']					= clang_pp_path
-#	CONFIG['CXX']					= clang_pp_path
-#	CONFIG['LDSHARED']		= 'llvm-link -shared'
-#	CONFIG['LDSHAREDXX']	= 'llvm-link -shared'
-#	
-#	CONFIG['CXXFLAGS']	+= ' -O4'
-#	$LDFLAGS	= '-O4'
-#	
-#else
-#	CONFIG['CXXFLAGS'] += ' -O3'
-#end
+if clang_path = find_executable('clang') and clang_pp_path = find_executable('clang++')
+	CONFIG['CC']	= clang_path
+	CONFIG['CXX']	= clang_pp_path
+end
 
 # Necessary header files.
 find_header('ruby/config.h') 
 
 # List the source files that need to be compiled.
 $srcs = [
-	'nmatrix.cpp',
+#	'nmatrix.cpp',
 	'ruby_constants.cpp',
 	
 	'data/data.cpp',
@@ -64,3 +53,4 @@ $objs = $srcs.map { |f| f.sub('.cpp', '.o') }
 
 # Create the actual Makefile.
 create_makefile('NMatrix')
+
