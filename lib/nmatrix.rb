31f2f59fa1ef4cfc976474be04fa86ea3798d881
Carlos Agarie <carlos.agarie@gmail.com>
2012-07-31 19:58:47 -0300

    Added lots of shortcut methods and an in-line constructor to lib/nmatrix.rb.
    All thanks to Daniel Carrera, who submitted it on SciRuby's mailing-list.

--- lib/nmatrix.rb
+++ lib/nmatrix.rb
@@ -219,7 +219,65 @@ protected
 
 end
 
+#######################################
+#
+# SIMPLE IN-LINE CONSTRUCTOR
+#
+# by Daniel Carrera <dcarrera@hush.com>
+#
+#######################################
+# 
+# NMatrix needs to have a succinct way to create a matrix by specifying
+# the components directly. This is very usefeul for using NMatrix as an
+# advanced calculator, it is useful for learning NMatrix and it is also
+# useful for testing language features or developing algorithms.
+# 
+# The N[] function provides a way to create a matrix in a way that is
+# very short and very natural, simply by specifying the components in
+# the traditional Ruby array syntax.  Optionaly, one can specify a
+# dtype as the last parameter (default is :float64).
+# 
+# a = N[ 1,2,3,4 ]          =>  1.0  2.0  3.0  4.0
+# 
+# a = N[ 1,2,3,4, :int32 ]  =>  1  2  3  4
+# 
+# a = N[ [1,2,3], [3,4,5] ] =>  1.0  2.0  3.0
+#                               3.0  4.0  5.0
+# 
+# 
+# SYNTAX COMPARISON:
+# 
+#     MATLAB:		a = [ [1 2 3] ; [4 5 6] ]   or  [ 1 2 3 ; 4 5 6 ]
+#     IDL:			a = [ [1,2,3] , [4,5,6] ]
+#     NumPy:		a = array( [1,2,3], [4,5,6] )
+#     
+#     SciRuby:      a = N[ [1,2,3], [4,5,6] ]
+#     Ruby array:   a =  [ [1,2,3], [4,5,6] ]
+#
 
+class N
+
+	def N.[](*params)
+		dtype = params.last.is_a?(Symbol) ? params.pop : :float64
+		
+		#
+		# First find the dimensions of the array.
+		#
+		i = 0
+		dim = []
+		foo = params
+		while foo.is_a?(Array)
+			dim[i] = foo.length
+			foo    = foo[0]
+			i += 1
+		end
+		
+		#
+		# Then flatten the array.
+		#
+		NMatrix.new( dim, params.flatten, dtype )
+	end
+end
 
 # This is a specific type of NMatrix in which only one dimension is not 1. Although it is stored as a rank-2, n x 1,
 # matrix, it acts as a rank-1 vector of size n. If the @orientation flag is set to :row, it is stored as 1 x n instead
@@ -286,4 +344,178 @@ protected
     ary
   end
 
+end
+
+#######################################
+#
+# Shortcuts to initialize common matrices.
+#
+# by Daniel Carrera <dcarrera@hush.com>
+#
+##########################################
+
+# zeros() or zeroes()
+#    
+#     Creates a new matrix of zeros with the dimensions supplied as
+#     parameters. Optional parameters include:
+#     
+#     * A storage type as the first parameter (default is :dense).
+#     * A dtype as the last parameter (default is :float64).
+#     
+# Examples:
+#   
+#   zeros(3) =>  0.0   0.0   0.0
+#
+#   zeros(2,3,:int32) =>  0  0  0
+#                         0  0  0
+#   
+#   zeros(:list, 5, :int32) =>  0  0  0  0  0
+#
+
+def zeros(*params)
+	dtype = params.last.is_a?( Symbol) ? params.pop   : :float64
+	store = params.first.is_a?(Symbol) ? params.shift : :dense
+	
+	NMatrix.new(store, params, 0, dtype)
+end
+
+alias :zeroes :zeros
+
+# ones()
+#    
+#     Creates a :dense matrix of ones with the dimensions supplied
+#     as parameters. Optionaly, one can specify a dtype as the last
+#     parameter (default is :float64).
+#     
+# Examples:
+#   
+#   ones(3) =>  1.0   1.0   1.0
+#
+#   ones(2,3,:int32) =>  1  1  1
+#                        1  1  1
+#
+
+def ones(*params)
+    dtype = params.last.is_a?( Symbol) ? params.pop   : :float64
+    
+    NMatrix.new(params, 1, dtype)
+end
+
+# identity() or eye()
+#      
+#     Creates an identitiy matrix (square matrix rank 2) of the size
+#     supplied as a parameter. Optional parameters include:
+#      
+#     * A storage type as the first parameter (default is :dense).
+#     * A dtype as the last parameter (default is :float64).
+#
+# Examples:
+#      
+#    eye(3) =>   1.0   0.0   0.0
+#                0.0   1.0   0.0
+#                0.0   0.0   1.0
+#    
+#    eye(3, :int32) =>   1   0   0
+#                        0   1   0
+#                        0   0   1
+#    
+#    eye(:yale, 2, :int32) =>   1   0
+#                               0   1
+#
+
+def eye(*params)
+	dtype = params.last.is_a?( Symbol) ? params.pop   : :float64
+	store = params.first.is_a?(Symbol) ? params.shift : :dense
+	
+	n = params[0]
+	m = zeros(store,n,n,dtype)
+	(0..n-1).each { |i| m[i,i] = 1 }
+	m
+end
+
+alias :identity :eye
+
+# seq()
+#      
+#     Creates a :dense NMatrix with a sequence of integers starting at
+#     zero until the matrix is filled. The parameters to the function
+#     are the dimensions of the matrix. Optionaly, one can specify a
+#     dtype as the last parameter (default is :float64).
+#     
+# Examples:
+# 
+#   seq(4) =>   0.0   1.0   2.0   3.0
+#
+#   seq(3,3, :int32) =>  0  1  2
+#                        3  4  5
+#                        6  7  8
+#
+
+def seq(*params)
+	dtype = params.last.is_a?( Symbol) ? params.pop   : :float64
+	
+	prod = 1
+	params.each { |i| prod *= i }
+	
+	NMatrix.new(params,  (0..prod-1).to_a, dtype )
+end
+
+######################################
+#
+# FUNCIONS FOR MATLAB AND IDL REFUGEES
+#
+######################################
+#
+# These are functions that replicate existing functionality, but
+# would probably be appreciated by MATLAB or IDL users.
+#
+
+# indgen() , findgen() , bindgen() , cindgen()
+#      
+#      These IDL functions are similar to seq() but less flexile.
+#      They produce on-dimensional matrices:
+#      
+#      indgen    -- Integer vector   --   seq(n, :int32)
+#      findgen   -- Float vector     --   seq(n, :float32)
+#      bindgen   -- Byte vector      --   seq(n, :byte)
+#      cindgen   -- Complex vector   --   seq(n, :complex64)
+#      
+
+def indgen(n)
+	seq(n, :int32)
+end
+
+def findgen(n)
+	seq(n, :float32)
+end
+
+def bindgen(n)
+	seq(n, :byte)
+end
+
+def cindgen(n)
+	seq(n, :complex64)
+end
+
+# linspace()
+#      
+#      This MATLAB function somewhat resembles seq(), but it differs
+#      enough that is likely to be legitimately useful to non-MATLAB
+#      refugees. This function takes three parameter, the last one
+#      being an integer.
+#      
+#      linspace( a, b, n )
+#      
+#      This returns a vector with n values from a to b.
+#      
+# Ex:  x = linspace(0, pi, 1000)
+#      y = sin(x)
+#
+
+def linspace(a, b, nsteps)
+	#
+	# Algorithm:  seq(n) * (b-a)/(n-1) + a
+	#
+	step = (b-a) * 1.0 / (nsteps - 1)
+	seq(nsteps) * NMatrix.new([nsteps], step) + NMatrix.new([nsteps], a)
 end
