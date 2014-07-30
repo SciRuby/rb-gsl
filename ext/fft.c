12b24b60fe5719ce37222eed8f4c3d0cf7e2d6b0
Paul Buonopane <paul+git@earth2me.com>
2014-07-30 06:08:35 -0400

    Fixes SciRuby/rb-gsl#7: undefined constants

--- ext/fft.c
+++ ext/fft.c
@@ -907,10 +907,10 @@ void Init_gsl_fft(VALUE module)
 
   /*****/
 
-  rb_define_const(mgsl_fft, "Forward", INT2FIX(forward));
-  rb_define_const(mgsl_fft, "FORWARD", INT2FIX(forward));
-  rb_define_const(mgsl_fft, "Backward", INT2FIX(backward));
-  rb_define_const(mgsl_fft, "BACKWARD", INT2FIX(backward));
+  rb_define_const(mgsl_fft, "Forward", INT2FIX(gsl_fft_forward));
+  rb_define_const(mgsl_fft, "FORWARD", INT2FIX(gsl_fft_forward));
+  rb_define_const(mgsl_fft, "Backward", INT2FIX(gsl_fft_backward));
+  rb_define_const(mgsl_fft, "BACKWARD", INT2FIX(gsl_fft_backward));
 
   /* Transforms for complex vectors */
   rb_define_method(cgsl_vector_complex, "radix2_forward", 
