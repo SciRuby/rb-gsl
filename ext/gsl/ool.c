cac0bf35f785603981c6c179dc835669af17b395
Jens Wille <jens.wille@gmail.com>
2014-11-17 13:18:27 +0100

    ext/gsl/ool.c: Ruby 2.x compatibility. (3885ae6)

--- ext/gsl/ool.c
+++ ext/gsl/ool.c
@@ -770,7 +770,7 @@ static VALUE rb_ool_conmin_pgrad_parameters_default(VALUE klass)
 	VALUE ary;
 	ool_conmin_parameters_default(ool_conmin_minimizer_pgrad, (void*) &P);
 	ary = create_parameters_ary_pgrad(&P);
-	RBASIC(ary)->klass = cool_conmin_pgrad_parameters;
+	RBGSL_SET_CLASS(ary, cool_conmin_pgrad_parameters);
 	return ary;
 }
 
@@ -780,7 +780,7 @@ static VALUE rb_ool_conmin_spg_parameters_default(VALUE klass)
 	VALUE ary;
 	ool_conmin_parameters_default(ool_conmin_minimizer_spg, (void*) &P);
 	ary = create_parameters_ary_spg(&P);	
-	RBASIC(ary)->klass = cool_conmin_spg_parameters;	
+	RBGSL_SET_CLASS(ary, cool_conmin_spg_parameters);	
 	return ary;	
 }
 
@@ -790,7 +790,7 @@ static VALUE rb_ool_conmin_gencan_parameters_default(VALUE klass)
 	VALUE ary;
 	ool_conmin_parameters_default(ool_conmin_minimizer_gencan, (void*) &P);
 	ary = create_parameters_ary_gencan(&P);		
-	RBASIC(ary)->klass = cool_conmin_gencan_parameters;	
+	RBGSL_SET_CLASS(ary, cool_conmin_gencan_parameters);	
 	return ary;
 }
 
