88cdfda28bfa87398185216a7586efa5a3a10e04
Veselin Vasilev <vesselinv@me.com>
2014-12-14 00:08:17 -0600

    Optimize and extract multiplication and division operations on Fixnum and Float
    Signed-off-by: Veselin Vasilev <vesselinv@me.com>

--- lib/gsl/oper.rb
+++ lib/gsl/oper.rb
@@ -1,68 +1,42 @@
-class Fixnum
-  alias :_orig_mul :*
-  alias :_orig_div :/
-  def *(other)
-    if other.kind_of?(GSL::Matrix) or other.kind_of?(GSL::Vector) or other.kind_of?(GSL::Matrix::Int) or other.kind_of?(GSL::Vector::Int) or other.kind_of?(GSL::Vector::Complex)or other.kind_of?(GSL::Matrix::Complex)
-      other.scale(self)
-    else
-      if GSL.have_tensor?
-        if other.kind_of?(GSL::Tensor) or other.kind_of?(GSL::Tensor::Int)
+module GSL::Oper
+  def self.included base
+    base.class_eval do
+
+      alias :_orig_mul :*
+      alias :_orig_div :/
+
+      def *(other)
+        return _orig_mul(other) if other.is_a?(Numeric)
+
+        if other.is_a?(GSL::Matrix) || other.is_a?(GSL::Vector) || other.is_a?(GSL::Matrix::Int) ||
+           other.is_a?(GSL::Vector::Int) || other.is_a?(GSL::Vector::Complex) || other.is_a?(GSL::Matrix::Complex)
+          other.scale(self)
+        elsif GSL.have_tensor? && (other.is_a?(GSL::Tensor) || other.is_a?(GSL::Tensor::Int))
           other.scale(self)
         else
-          self._orig_mul(other)
+          _orig_mul(other)
         end
-      else
-        self._orig_mul(other)
       end
-    end
-  end
 
-  def /(other)
-    if other.kind_of?(GSL::Poly) or other.kind_of?(GSL::Poly::Int)
-      a = GSL::Poly[1]; a[0] = self
-      GSL::Rational.new(a, other)
-    elsif other.kind_of?(GSL::Vector::Col) 
-      other.scale(1.0/GSL::pow_2(other.dnrm2))
-    elsif other.kind_of?(GSL::Vector::Int::Col)
-      v = other.to_f
-      v.scale(1.0/GSL::pow_2(v.dnrm2))
-    else
-      self._orig_div(other)
+      def /(other)
+        return _orig_div(other) if other.is_a?(Numeric)
+
+        if other.is_a?(GSL::Poly) || other.is_a?(GSL::Poly::Int)
+          a = GSL::Poly[1]; a[0] = self
+          GSL::Rational.new(a, other)
+        elsif other.is_a?(GSL::Vector::Col)
+          other.scale(1.0/GSL::pow_2(other.dnrm2))
+        elsif other.is_a?(GSL::Vector::Int::Col)
+          v = other.to_f
+          v.scale(1.0/GSL::pow_2(v.dnrm2))
+        else
+          _orig_div(other)
+        end
+      end
+
     end
   end
 end
 
-class Float
-  alias :_orig_mul :*
-  alias :_orig_div :/
-
-  def *(other)
-    if other.kind_of?(GSL::Matrix) or other.kind_of?(GSL::Vector) or other.kind_of?(GSL::Matrix::Int) or other.kind_of?(GSL::Vector::Int) or other.kind_of?(GSL::Vector::Complex)or other.kind_of?(GSL::Matrix::Complex)
-      other.scale(self)
-    else
-      if GSL.have_tensor?
-        if other.kind_of?(GSL::Tensor) or other.kind_of?(GSL::Tensor::Int)
-          other.scale(self)
-        else
-          self._orig_mul(other)
-        end
-      else
-        self._orig_mul(other)
-      end
-    end
-  end
-
-  def /(other)
-    if other.kind_of?(GSL::Poly) or other.kind_of?(GSL::Poly::Int)
-      a = GSL::Poly[1]; a[0] = self
-      GSL::Rational.new(a, other)
-    elsif other.kind_of?(GSL::Vector::Col) 
-      other.scale(1.0/GSL::pow_2(other.dnrm2))
-    elsif other.kind_of?(GSL::Vector::Int::Col)
-      v = other.to_f
-      v.scale(1.0/GSL::pow_2(v.dnrm2))
-    else
-      self._orig_div(other)
-    end
-  end
-end
+Fixnum.send(:include, GSL::Oper)
+Float.send(:include, GSL::Oper)
