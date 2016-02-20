8cccc89699912f7c39e66f31b46cf30e885bf895
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-02-21 01:10:43 +0530

    gsl-nmatrix conversion code compiling

--- ext/gsl_native/gsl_nmatrix.c
+++ ext/gsl_native/gsl_nmatrix.c
@@ -12,49 +12,50 @@ extern VALUE rb_nmatrix_dense_create(nm_dtype_t dtype, size_t* shape, size_t ran
 extern VALUE rb_nvector_dense_create(nm_dtype_t dtype, void* elements, size_t length);
 
 /* GSL::Vector -> NMatrix */
-static VALUE rb_gsl_vector_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_vector_to_nmatrix(VALUE obj) {
   gsl_vector *v = NULL;
   Data_Get_Struct(obj, gsl_vector, v);
 
   return rb_nvector_dense_create(FLOAT64, v->data, v->size);
 }
 
-static VALUE rb_gsl_vector_int_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_vector_int_to_nmatrix(VALUE obj) {
   gsl_vector_int *v = NULL;
   Data_Get_Struct(obj, gsl_vector_int, v);
 
   return rb_nvector_dense_create(INT32, v->data, v->size);
 }
 
-static VALUE rb_gsl_vector_complex_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_vector_complex_to_nmatrix(VALUE obj) {
   gsl_vector_complex *v = NULL;
   Data_Get_Struct(obj, gsl_vector_complex, v);
 
   return rb_nvector_dense_create(COMPLEX128, v->data, v->size);
 }
 
-static VALUE rb_gsl_matrix_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_matrix_to_nmatrix(VALUE obj) {
   gsl_matrix *m = NULL;
   Data_Get_Struct(obj, gsl_matrix, m);
 
   return rb_nmatrix_dense_create(FLOAT64, &(m->size1), 2, m->data, m->size1 * m->size2);
 }
 
-static VALUE rb_gsl_matrix_int_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_matrix_int_to_nmatrix(VALUE obj) {
   gsl_matrix_int *m = NULL;
   Data_Get_Struct(obj, gsl_matrix_int, m);
 
   return rb_nmatrix_dense_create(INT64, &(m->size1), 2, m->data, m->size1 * m->size2);
 }
 
-static VALUE rb_gsl_matrix_complex_to_nmatrix(VALUE obj, VALUE klass) {
+static VALUE rb_gsl_matrix_complex_to_nmatrix(VALUE obj) {
   gsl_matrix_complex *m = NULL;
   Data_Get_Struct(obj, gsl_matrix_complex, m);
 
   return rb_nmatrix_dense_create(COMPLEX128, &(m->size1), 2, m->data, m->size1 * m->size2);
 }
 
-gsl_vector* nm_to_gv(VALUE nm) {
+// functions called on NMatrix objects. 'nm' is of type NMatrix.
+gsl_vector* rb_gsl_nmatrix_to_gv(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
   // TODO: change this once nmatrix is fixed
   gsl_vector* v = gsl_vector_alloc( FIX2INT(rb_funcall(nm, rb_intern("count"), 0, NULL)) );
@@ -68,9 +69,10 @@ gsl_vector* nm_to_gv(VALUE nm) {
   return v;
 }
 
-gsl_vector_int* nm_to_gv_int(VALUE nm) {
+gsl_vector_int* rb_gsl_nmatrix_to_gv_int(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
-  gsl_vector_int* v = gsl_vector_int_alloc( FIX2INT(rb_funcall(nm, rb_intern("count"), 0, NULL)) );
+  gsl_vector_int* v = gsl_vector_int_alloc(
+    FIX2INT(rb_funcall(nm, rb_intern("count"), 0, NULL)));
 
   if (s->dtype != INT32) {
     rb_raise(nm_eDataTypeError, "requires dtype of :int32 to convert to a GSL int vector");
@@ -81,9 +83,10 @@ gsl_vector_int* nm_to_gv_int(VALUE nm) {
   return v;
 }
 
-gsl_vector_complex* nm_to_gv_complex(VALUE nm) {
+gsl_vector_complex* rb_gsl_nmatrix_to_gv_complex(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
-  gsl_vector_complex* v = gsl_vector_complex_alloc( FIX2INT(rb_funcall(nm, rb_intern("count"), 0, NULL)) );
+  gsl_vector_complex* v = gsl_vector_complex_alloc(
+    FIX2INT(rb_funcall(nm, rb_intern("count"), 0, NULL)) );
 
   if (s->dtype != COMPLEX128) {
     rb_raise(nm_eDataTypeError, "requires dtype of :complex128 to convert to a GSL complex vector");
@@ -94,7 +97,27 @@ gsl_vector_complex* nm_to_gv_complex(VALUE nm) {
   return v;
 }
 
-gsl_matrix* nm_to_gm(VALUE nm) {
+// NMatrix function to convert NMatrix to GSL::Vector object depending on dtype
+VALUE rb_gsl_nmatrix_to_gsl_vector_method(VALUE nm) {
+  if (NM_DTYPE(nm) == COMPLEX64 || NM_DTYPE(nm) == COMPLEX128) {
+    return Data_Wrap_Struct(cgsl_vector_complex, 0, gsl_vector_complex_free, 
+      rb_gsl_nmatrix_to_gv_complex(nm));
+  } 
+  else if (NM_DTYPE(nm) == INT32) {
+    return Data_Wrap_Struct(cgsl_vector_int, 0, gsl_vector_int_free,
+      rb_gsl_nmatrix_to_gv_int(nm));
+  }
+  else if (NM_DTYPE(nm) == FLOAT64) {
+    return Data_Wrap_Struct(cgsl_vector, 0, gsl_vector_free,
+      rb_gsl_nmatrix_to_gv(nm));
+  }
+  else {
+    rb_raise(nm_eDataTypeError, 
+      "NMatrix should be either :complex64, :complex128, :int32 or :float64 type."); 
+  }
+}
+
+gsl_matrix* rb_gsl_nmatrix_to_gm(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
   gsl_matrix* m = gsl_matrix_alloc( s->shape[0], s->shape[1] );
 
@@ -106,7 +129,7 @@ gsl_matrix* nm_to_gm(VALUE nm) {
   return m;
 }
 
-gsl_matrix_int* nm_to_gm_int(VALUE nm) {
+gsl_matrix_int* rb_gsl_nmatrix_to_gm_int(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
   gsl_matrix_int* m = gsl_matrix_int_alloc( s->shape[0], s->shape[1] );
 
@@ -118,7 +141,7 @@ gsl_matrix_int* nm_to_gm_int(VALUE nm) {
   return m;
 }
 
-gsl_matrix_complex* nm_to_gm_complex(VALUE nm) {
+gsl_matrix_complex* rb_gsl_nmatrix_to_gm_complex(VALUE nm) {
   NM_DENSE_STORAGE* s = NM_STORAGE_DENSE(nm);
   gsl_matrix_complex* m = gsl_matrix_complex_alloc( s->shape[0], s->shape[1] );
 
@@ -130,65 +153,65 @@ gsl_matrix_complex* nm_to_gm_complex(VALUE nm) {
   return m;
 }
 
-VALUE rb_gsl_nm_to_gsl_vector(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_vector, 0, gsl_vector_free, nm_to_gv(n));
+VALUE rb_gsl_nmatrix_to_gsl_vector(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_vector, 0, gsl_vector_free,
+    rb_gsl_nmatrix_to_gv(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_vector_int(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_vector_int, 0, gsl_vector_int_free, nm_to_gv_int(n));
+VALUE rb_gsl_nmatrix_to_gsl_vector_int(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_vector_int, 0, gsl_vector_int_free,
+    rb_gsl_nmatrix_to_gv_int(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_vector_complex(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_vector_complex, 0, gsl_vector_complex_free, nm_to_gv_complex(n));
+VALUE rb_gsl_nmatrix_to_gsl_vector_complex(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_vector_complex, 0, gsl_vector_complex_free,
+    rb_gsl_nmatrix_to_gv_complex(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_matrix(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_matrix, 0, gsl_matrix_free, nm_to_gm(n));
+VALUE rb_gsl_nmatrix_to_gsl_matrix(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_matrix, 0, gsl_matrix_free,
+    rb_gsl_nmatrix_to_gm(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_matrix_int(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_matrix_int, 0, gsl_matrix_int_free, nm_to_gm_int(n));
+VALUE rb_gsl_nmatrix_to_gsl_matrix_int(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_matrix_int, 0, gsl_matrix_int_free,
+    rb_gsl_nmatrix_to_gm_int(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_matrix_complex(VALUE obj, VALUE n) {
-  return Data_Wrap_Struct(cgsl_matrix_complex, 0, gsl_matrix_complex_free, nm_to_gm_complex(n));
+VALUE rb_gsl_nmatrix_to_gsl_matrix_complex(VALUE obj, VALUE n) {
+  return Data_Wrap_Struct(cgsl_matrix_complex, 0, gsl_matrix_complex_free,
+    rb_gsl_nmatrix_to_gm_complex(n));
 }
 
-VALUE rb_gsl_nm_to_gsl_vector_method(VALUE nm) {
-  VALUE v;
-
-  if (NM_DTYPE(nm) == COMPLEX64 || NM_DTYPE(nm) == COMPLEX128) {
-    return Data_Wrap_Struct(cgsl_vector_complex, 0, gsl_vector_complex_free, nm_to_gv_complex(nm));
-  } 
-  else if (NM_DTYPE(nm) == INT32) {
-    return Data_Wrap_Struct(cgsl_vector_int, 0, gsl_vector_int_free, nm_to_gv_int(nm));
-  }
-  else {
-    return Data_Wrap_Struct(cgsl_vector, 0, gsl_vector_free, nm_to_gv(nm));
-  }
-
-  return v;
-}
-
-Init_gsl_nmatrix(VALUE module)
-{
+void Init_gsl_nmatrix(VALUE module)
+{ 
+  // functions to convert GSL::Vector to 1D NMatrix.
   rb_define_method(cgsl_vector, "to_nm", rb_gsl_vector_to_nmatrix, 0);
   rb_define_method(cgsl_vector_int, "to_nm", rb_gsl_vector_int_to_nmatrix, 0);
   rb_define_method(cgsl_vector_complex, "to_nm", rb_gsl_vector_complex_to_nmatrix, 0);
 
-  rb_define_singleton_method(cgsl_vector, "nm_to_gslv", rb_gsl_nm_to_gsl_vector, 1);
-  rb_define_singleton_method(cgsl_vector_int, "nm_to_gslv", rb_gsl_nm_to_gsl_vector_int, 1);
-  rb_define_singleton_method(cgsl_vector_complex, "nm_to_gslv", rb_gsl_nm_to_gsl_vector_complex, 1);
-  
+  // functions to convert GSL::Matrix to 2D Nmatrix.
   rb_define_method(cgsl_matrix, "to_nm", rb_gsl_matrix_to_nmatrix, 0);
   rb_define_method(cgsl_matrix_int, "to_nm", rb_gsl_matrix_int_to_nmatrix, 0);
   rb_define_method(cgsl_matrix_complex, "to_nm", rb_gsl_matrix_complex_to_nmatrix, 0);
 
-  rb_define_singleton_method(cgsl_matrix, "nm_to_gslm",  rb_gsl_nm_to_gsl_matrix, 1);
-  rb_define_singleton_method(cgsl_matrix_int, "nm_to_gslm",  rb_gsl_nm_to_gsl_matrix_int, 1);
-  rb_define_singleton_method(cgsl_matrix_complex, "nm_to_gslm",  rb_gsl_nm_to_gsl_matrix_complex, 1);
+  // class functions to convert NMatrix to GSL::Vector
+  rb_define_singleton_method(cgsl_vector, "nm_to_gslv",
+    rb_gsl_nmatrix_to_gsl_vector, 1);
+  rb_define_singleton_method(cgsl_vector_int, "nm_to_gslv",
+    rb_gsl_nmatrix_to_gsl_vector_int, 1);
+  rb_define_singleton_method(cgsl_vector_complex, "nm_to_gslv",
+    rb_gsl_nmatrix_to_gsl_vector_complex, 1);
+  
+  // class functions to convert NMatrix to GSL::Matrix
+  rb_define_singleton_method(cgsl_matrix, "nm_to_gslm", 
+    rb_gsl_nmatrix_to_gsl_matrix, 1);
+  rb_define_singleton_method(cgsl_matrix_int, "nm_to_gslm", 
+    rb_gsl_nmatrix_to_gsl_matrix_int, 1);
+  rb_define_singleton_method(cgsl_matrix_complex, "nm_to_gslm",
+    rb_gsl_nmatrix_to_gsl_matrix_complex, 1);
 
-  rb_define_method(cNMatrix, "to_gslv", rb_gsl_nm_to_gsl_vector_method, 0);
+  rb_define_method(cNMatrix, "to_gslv", rb_gsl_nmatrix_to_gsl_vector_method, 0);
 }
 
 #endif
--- ext/gsl_native/include/rb_gsl_with_nmatrix.h
+++ ext/gsl_native/include/rb_gsl_with_nmatrix.h
@@ -8,7 +8,37 @@
 #include "nmatrix.h"
 #include "include/rb_gsl_array.h"
 extern VALUE cNMatrix;
+extern VALUE nm_eDataTypeError, nm_eStorageTypeError;
 
+// functions to convert GSL::Vectors to 1D NMatrix
+static VALUE rb_gsl_vector_to_nmatrix(VALUE obj);
+static VALUE rb_gsl_vector_int_to_nmatrix(VALUE obj);
+static VALUE rb_gsl_vector_complex_to_nmatrix(VALUE obj);
+
+// functions to convert GSL::Matrix to 2D NMatrix
+static VALUE rb_gsl_matrix_to_nmatrix(VALUE obj);
+static VALUE rb_gsl_matrix_int_to_nmatrix(VALUE obj);
+static VALUE rb_gsl_matrix_complex_to_nmatrix(VALUE obj);
+
+// functions to convert NMatrix to GSL::Vector
+gsl_vector* rb_gsl_nmatrix_to_gv(VALUE nm);
+gsl_vector_int* rb_gsl_nmatrix_to_gv_int(VALUE nm);
+gsl_vector_complex* rb_gsl_nmatrix_to_gv_complex(VALUE nm);
+
+// functions to convert NMatrix to GSL::Matrix
+gsl_matrix* rb_gsl_nmatrix_to_gm(VALUE nm);
+gsl_matrix_int* rb_gsl_nmatrix_to_gm_int(VALUE nm);
+gsl_matrix_complex* rb_gsl_nmatrix_to_gm_complex(VALUE nm);
+
+// singleton functions to convert NMatrix to GSL::Vector
+VALUE rb_gsl_nmatrix_to_gsl_vector(VALUE obj, VALUE nmatrix);
+VALUE rb_gsl_nmatrix_to_gsl_vector_int(VALUE obj, VALUE nmatrix);
+VALUE rb_gsl_nmatrix_to_gsl_vector_complex(VALUE obj, VALUE nmatrix);
+
+// singleton functions to convert NMatrix to GSL::Matrix
+VALUE rb_gsl_nmatrix_to_gsl_matrix(VALUE obj, VALUE nmatrix);
+VALUE rb_gsl_nm_to_gsl_matrix_int(VALUE obj, VALUE nmatrix);
+VALUE rb_gsl_nmatrix_to_gsl_matrix_complex(VALUE obj, VALUE nmatrix);
 
 #endif // HAVE_NMATRIX_H
 #endif // RB_GSL_WITH_NMATRIX_H
