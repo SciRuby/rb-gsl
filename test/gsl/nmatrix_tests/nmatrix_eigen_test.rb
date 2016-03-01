bd00194fd8995c72bb4563d2d99d00c1ff4cf689
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-03-02 01:39:46 +0530

    nmatrix works with eigen

--- test/gsl/nmatrix_tests/nmatrix_eigen_test.rb
+++ test/gsl/nmatrix_tests/nmatrix_eigen_test.rb
@@ -18,11 +18,11 @@ class NMatrixEigenTest < GSL::TestCase
        0.322416,-0.509579,-0.100228,-0.791411 , 
        0.252161,-0.514048,-0.638283, 0.514553], dtype: :float64)
     
-    assert_rel GSL::Eigen.symm(@nmatrix), eigen_values, 0.001, "GSL::Eigen.symm(nmatrix)"
+    assert_enum_abs GSL::Eigen.symm(@nmatrix), eigen_values, 0.001, "GSL::Eigen.symm(nmatrix)"
 
     val, vec = GSL::Eigen.symmv(@nmatrix)
 
-    assert_rel val, eigen_values , 0.001, "GSL::Eigen.symmv(nmatrix)"
-    assert_rel val, eigen_vectors, 0.001, "GSL::Eigen.symmv(nmatrix)"
+    assert_enum_abs val, eigen_values , 0.001, "GSL::Eigen.symmv(nmatrix)"
+    assert_enum_abs vec, eigen_vectors, 0.001, "GSL::Eigen.symmv(nmatrix)"
   end
 end
