ee6d65b5f8808d2d0fa746a770692c2bbaa7bb46
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-03-02 12:28:03 +0530

    nmatrix works with cheb and functions

--- test/gsl/nmatrix_tests/nmatrix_cheb_test.rb
+++ test/gsl/nmatrix_tests/nmatrix_cheb_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require_relative '../../test_helper'
 
 class NMatrixChebTest < GSL::TestCase
   def test_eval
@@ -17,18 +17,18 @@ class NMatrixChebTest < GSL::TestCase
 
     ff = f.eval(x)
 
-    assert ff.class, NMatrix
+    assert ff.class == NMatrix
 
     cs.init(f, 0, 1)
     r10 = cs.eval_n(10, x)
     r40 = cs.eval(x)
 
-    assert r10.class, NMatrix
-    assert_rel r10.last, 0.758879, 0.001
-    assert_rel r10[5]  , 0.247816, 0.001
+    assert r10.class == NMatrix
+    assert_rel r10.to_a.last, 0.758879, 0.001, 'test r10.last'
+    assert_rel r10[5]  , 0.247816, 0.001, 'test r10[5]'
 
-    assert r40.class, NMatrix
-    assert_rel r40[5]   , 0.255682, 0.001
-    assert_rel r40.first, 0.25633 , 0.001
+    assert r40.class == NMatrix
+    assert_rel r40[5]   , 0.255682, 0.001, 'test r40[5]'
+    assert_rel r40.first, 0.25633 , 0.001, 'test r40.first'
   end
 end
